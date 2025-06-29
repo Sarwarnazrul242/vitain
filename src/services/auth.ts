047cb968b727770d22b489790f0e05aed786336d
Osestic <agholor2004@gmail.com>
2025-06-28 23:05:44 -0400

    Connected the authentication code to the firestore database. Login and Signup db process flow works. Added form validation feedback for users with red text.

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,9 +1,13 @@
+import { reactive } from 'vue';
 import { initializeApp } from "firebase/app";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { sendEmailVerification } from "firebase/auth";
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
@@ -14,20 +18,38 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APPID,
 };
 
+
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth();
 
-export async function signup(email: string, password: string) {
+const db = getFirestore(app);
+
+export async function signup(firstName: string, lastName: string, email: string, password: string) {
+  console.log("Sign up clicked"); // Add this for debugging
+  
   try {
     let user_credentials = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
+    const user = user_credentials.user
+    
+    await sendEmailVerification(user);
 
-    console.log(user_credentials.user);
+    await setDoc(doc(db, "users", user.uid),{
+      uid: user.uid,
+      firstName: firstName,
+      lastName: lastName,
+      email: user.email,
+      createdAt: new Date().toISOString(),
+    });
+
+     console.log("User signed up and save: ", user);
+
   } catch (err) {
+    console.error("Sign up error: ", err)  //throw the error make the user try again comment?
     throw err;
   }
 }
@@ -41,10 +63,42 @@ export async function login(email: string, password: string) {
       email,
       password
     );
+    const user = user_credentials.user;
+
+    if (!user.emailVerified)
+    {
+      throw new Error("Please verify your email before logging in.");
+    }
+
 
     console.log(user_credentials.user);
   } catch (err) {
-    throw err;
+    if (err.message.includes("auth/invalid-email"))
+     {
+      throw {type: "email", message: "Invalid email"}
+    }
+    else if (err.message.includes("auth/missing-password"))
+    {
+      throw {type: "password", message: "Password required"}
+    }
+
+    else if (err.message.includes("auth/invalid-credential"))
+    {
+      throw {type: "password", message: "Wrong password. Try again or "+ 
+           "click Forgot password to reset it."}
+    }
+
+    else if (err.message.includes("auth/too-many-requests"))
+    {
+      throw {type: "login", message: "Too many login attempts. Try again later."}
+    }
+    
+    else
+    {
+        throw {type: "general", message: "Login error occured. Try again."}
+
+    }
+     
   }
 }
 
